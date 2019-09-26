29a4cfac00fa59e6ed5312774ed0b279a989bdf5
dds7@gatech.edu <29868036+dds1999@users.noreply.github.com>
2019-09-25 21:37:16 -0400

    Added Game Menu

---
+++ SolarFlare/Assets/Scripts/HUD Scripts/GameStarter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("MainScene");
+    }
+}
