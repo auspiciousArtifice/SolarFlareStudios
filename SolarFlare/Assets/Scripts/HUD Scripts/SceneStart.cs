ed7f2f1ed3a80234ad0c5461ececa8de21c8c737
destinideinde <destini.deindesmith@gmail.com>
2019-09-25 21:37:16 -0400

    Added Game Menu

---
+++ SolarFlare/Assets/Scripts/HUD Scripts/SceneStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Time.timeScale = 1f;
+    }
+
+}
