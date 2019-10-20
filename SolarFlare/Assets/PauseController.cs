2909a8b240f056ca2181b7559a4999449ab2b037
destinideinde <40705170+destinideinde@users.noreply.github.com>
2019-10-20 01:49:09 -0400

    in game menu, added instructions in last push

---
+++ SolarFlare/Assets/PauseController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null) Debug.LogError("No CanvasGroup found.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (canvasGroup.interactable)
+            {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.alpha = 0f;
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.alpha = 1f;
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
