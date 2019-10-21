a8c9fbc69c5b6a162ee6b520e3b3d0fc18a66313
Ben <benle2486@gmail.com>
2019-10-20 20:37:30 -0400

    Added bridge animation when button is collided with.

---
+++ SolarFlare/Assets/DrawBridge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBridge : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject bridge;
+    private Animator bridgeAnimator;
+
+    void Start()
+    {
+        bridgeAnimator = bridge.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bridge is drawing");
+        bridgeAnimator.SetBool("ButtonPressed", true);
+    }
+}
