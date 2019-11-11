ab94051492bf1e2cbcee7f3ccf18235081ab86c1
destinideinde <40705170+destinideinde@users.noreply.github.com>
2019-11-10 23:38:24 -0500

    instructions scene updates. how to put ai in this scene?

--- SolarFlare/Assets/Instructions_UI.cs
+++ SolarFlare/Assets/Instructions_UI.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Instructions_UI : MonoBehaviour
 {
     float levelTime;
+    float instructionTime;
     float LEVEL_TOTAL_TIME = 200;
-    float LEVEL_END_TIME = 0;
     int coinNum = 0;
-    /*
+    bool runTimer = false;
     GameObject textObj;
     GameObject timer;
     GameObject coinHud;
-    GameObject endScreen;
+    GameObject[] coins;
     GameObject character;
+    /*
+    GameObject endScreen;
     */
-    
+
     InstructionState state;
 
     public enum InstructionState
@@ -23,33 +27,34 @@ public class Instructions_UI : MonoBehaviour
         WelcomeInstruction,
         MoveCameraInstruction,
         MovePlayerInstruction,
-        SprintInstruction,
-        TimerInstruction,
+        GrappleInstruction,
         CoinInstruction,
         EvadeEnemiesInstruction,
-        FightEnemiesInstruction,
         ScoreInstruction
     };
-
-    // Start is called before the first frame update
 
     void Start()
     {
         state = InstructionState.WelcomeInstruction;
-        /*
         textObj = GameObject.FindGameObjectWithTag("Notifications");
         timer = GameObject.FindGameObjectWithTag("timer");
         coinHud = GameObject.FindGameObjectWithTag("coin_hud");
-        endScreen = GameObject.FindGameObjectWithTag("end_screen");
         character = GameObject.FindGameObjectWithTag("Player");
-
+        coins = GameObject.FindGameObjectsWithTag("PickUp");
         levelTime = LEVEL_TOTAL_TIME;
-
+        instructionTime = 0;
+        /*
+        endScreen = GameObject.FindGameObjectWithTag("end_screen");
+        */
+        Debug.Log(coins.Length);
+        for (int i = 0; i < coins.Length; i++)
+        {
+            coins[i].SetActive(false);
+        }
         if (textObj == null)
         {
             Debug.Log("Didn't find notifications bar");
         }
-        */
     }
 
     // Update is called once per frame
@@ -57,47 +62,94 @@ public class Instructions_UI : MonoBehaviour
     {
         switch (state) {
             case InstructionState.WelcomeInstruction :
-
+                if (instructionTime < 5)
+                {
+                    textObj.GetComponent<Text>().text = "Welcome to SolarFlare!";
+                }
+                else if (instructionTime < 10)
+                {
+                    textObj.GetComponent<Text>().text = "Race to the level endpoint before time runs out by navigating floating islands!";
+                    runTimer = true;
+                } else if (instructionTime < 15)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.MoveCameraInstruction;
+                }
                 break;
 
             case InstructionState.MoveCameraInstruction :
-
+                textObj.GetComponent<Text>().text = "Move your mouse to rotate the game camera.";
+                if (instructionTime > 5)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.MovePlayerInstruction;
+                }
                 break;
 
             case InstructionState.MovePlayerInstruction :
-
+                textObj.GetComponent<Text>().text = "Use arrows to move your game character and space to jump. Using shift will allow you to sprint.";
+                if (instructionTime > 10)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.GrappleInstruction;
+                }
                 break;
 
-            case InstructionState.SprintInstruction :
-
-                break;
-
-            case InstructionState.TimerInstruction :
-
+            case InstructionState.GrappleInstruction:
+                textObj.GetComponent<Text>().text = "Click to launch a grappling hook then click again to stop retracting and start swinging. A third click will release you.";
+                if (instructionTime > 15)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.CoinInstruction;
+                }
                 break;
 
             case InstructionState.CoinInstruction :
-
+                textObj.GetComponent<Text>().text = "Collect coins along the way to add to your final score!";
+                for (int i = 0; i < coins.Length; i++)
+                {
+                    coins[i].SetActive(true);
+                }
+                if (instructionTime > 10)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.EvadeEnemiesInstruction;
+                }
                 break;
 
             case InstructionState.EvadeEnemiesInstruction :
-
+                textObj.GetComponent<Text>().text = "You might also have to fight or evade enemies";
+                if (instructionTime > 10)
+                {
+                    instructionTime = 0;
+                    state = InstructionState.ScoreInstruction;
+                }
                 break;
 
-            case InstructionState.FightEnemiesInstruction :
-
-                break;
 
             case InstructionState.ScoreInstruction :
-
+                textObj.GetComponent<Text>().text = "Good Luck!!";
+                if (instructionTime > 10)
+                {
+                    instructionTime = 0;
+                    SceneManager.LoadScene("MainScene");
+                }
                 break;
 
         }
+        if (runTimer)
+        {
+            levelTime -= Time.deltaTime;
+        }
+        instructionTime += Time.deltaTime;
+
+        timer = GameObject.FindGameObjectWithTag("timer");
+        if (timer != null) timer.GetComponent<Text>().text = ((int)levelTime).ToString();
 
         /*
         // this shouldn't be necessary but... it was buggy so here we are
         textObj = GameObject.FindGameObjectWithTag("Notifications");
-        timer = GameObject.FindGameObjectWithTag("timer");
+       
         coinHud = GameObject.FindGameObjectWithTag("coin_hud");
         endScreen = GameObject.FindGameObjectWithTag("end_screen");
         character = GameObject.FindGameObjectWithTag("Player");
@@ -107,7 +159,7 @@ public class Instructions_UI : MonoBehaviour
 
         // makle the text updates
         if (textObj != null) textObj.GetComponent<Text>().text = message;
-        if (timer != null) timer.GetComponent<Text>().text = ((int)levelTime).ToString();
+        
         if (character != null) coinNum = character.GetComponent<Coin_Counter>().getCoinCount() * 5;
         if (coinHud != null) coinHud.GetComponent<Text>().text = coinNum.ToString();
 
