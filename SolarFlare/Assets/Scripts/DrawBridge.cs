f201076ccde292bf42c3c797bf1522068214c467
Ben <benle2486@gmail.com>
2019-11-20 21:30:56 -0500

    Changed Button back to whenever player touches it to activate the bridge.

--- SolarFlare/Assets/Scripts/DrawBridge.cs
+++ SolarFlare/Assets/Scripts/DrawBridge.cs
@@ -48,7 +48,7 @@ public class DrawBridge : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Collider>().tag == "Player")
+        if (other.tag == "Player")
         {
             bridgeAnimator.SetBool("ButtonPressed", true);
         }
