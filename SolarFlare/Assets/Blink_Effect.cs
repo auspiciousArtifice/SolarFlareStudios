4d5a1a4f82fa40aa1ebaa9966138dd02be95a139
destinideinde <40705170+destinideinde@users.noreply.github.com>
2019-11-29 14:59:12 -0500

    Tab and backspace now navigate tthrough the instructions. Also, these instructions stay on the HUD throughout the insturctions scene

---
+++ SolarFlare/Assets/Blink_Effect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink_Effect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float blink_speed = .05f;
+    private bool subtract = true;
+    private float r;
+    private float b;
+    private float g;
+
+    void Start()
+    {
+        Text text = this.gameObject.GetComponent<Text>();
+        if (text != null)
+        {
+            r = text.color.r;
+            b = text.color.b;
+            g = text.color.g;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text text = this.gameObject.GetComponent<Text>();
+        if (text != null)
+        {
+            if (subtract)
+            {
+                text.color = new Color(r, b, g, text.color.a - blink_speed);
+            } else
+            {
+                text.color = new Color(r, b, g, text.color.a + blink_speed);
+            }
+            if (text.color.a <= .25 || text.color.a >= 1)
+            {
+                subtract = !subtract;
+            }
+            
+        }
+    }
+}
--- SolarFlare/Assets/Scripts/Instructions_UI.cs
+++ SolarFlare/Assets/Scripts/Instructions_UI.cs
@@ -9,19 +9,14 @@ public class Instructions_UI : MonoBehaviour
     float levelTime;
     float instructionTime;
     float LEVEL_TOTAL_TIME = 65;
-    //int coinNum = 0;
     bool runTimer = false;
+    int stateNum;
     GameObject textObj;
     GameObject timer;
-    //GameObject coinHud;
     GameObject[] coins;
-    //GameObject character;
 	bool ranOnce;
-
+    InstructionState[] states;
     public GameObject enemy;
-    /*
-    GameObject endScreen;
-    */
 
     InstructionState state;
 
@@ -37,9 +32,24 @@ public class Instructions_UI : MonoBehaviour
 		ScoreInstruction
     };
 
+   
+
     void Start()
     {
+        states = 
+            new InstructionState[]{
+            InstructionState.WelcomeInstruction,
+            InstructionState.MoveCameraInstruction,
+            InstructionState.MovePlayerInstruction,
+            InstructionState.GrappleInstruction,
+            InstructionState.CoinInstruction,
+            InstructionState.EvadeEnemiesInstruction,
+            InstructionState.AttackSwordInstruction,
+            InstructionState.ScoreInstruction};
+
         state = InstructionState.WelcomeInstruction;
+        stateNum = 0;
+
         textObj = GameObject.FindGameObjectWithTag("Notifications");
         timer = GameObject.FindGameObjectWithTag("timer");
         //coinHud = GameObject.FindGameObjectWithTag("coin_hud");
@@ -61,6 +71,11 @@ public class Instructions_UI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (stateNum < states.Length)
+        {
+            state = states[stateNum];
+        }
+       
         switch (state) {
             case InstructionState.WelcomeInstruction :
                 if (instructionTime < 5)
@@ -77,35 +92,18 @@ public class Instructions_UI : MonoBehaviour
                     textObj.GetComponent<Text>().text = "First we'll explain some quick instructions. When you're ready to move on, just press tab!";
                     runTimer = true;
                 }
-
-                if (Input.GetKeyDown("tab"))
-                {
-                    state = InstructionState.MoveCameraInstruction;
-                }
                 break;
 
             case InstructionState.MoveCameraInstruction :
                 textObj.GetComponent<Text>().text = "Move your mouse to rotate the game camera.";
-                if (Input.GetKeyDown("tab"))
-                {
-                   state = InstructionState.MovePlayerInstruction;
-                }
                 break;
 
             case InstructionState.MovePlayerInstruction :
                 textObj.GetComponent<Text>().text = "Use arrows to move your game character and space to jump. Using shift will allow you to sprint and alt will make you dash mid-air.";
-                if (Input.GetKeyDown("tab"))
-                {
-                    state = InstructionState.GrappleInstruction;
-                }
                 break;
 
             case InstructionState.GrappleInstruction:
                 textObj.GetComponent<Text>().text = "Click to launch a grappling hook and click again to release yourself! Q and E will shorten your grapple hook.";
-                if (Input.GetKeyDown("tab"))
-                {
-                    state = InstructionState.CoinInstruction;
-                }
                 break;
 
             case InstructionState.CoinInstruction :
@@ -120,7 +118,6 @@ public class Instructions_UI : MonoBehaviour
 				}
                 if (Input.GetKeyDown("tab"))
                 {
-                    state = InstructionState.EvadeEnemiesInstruction;
 					ranOnce = false;
 				}
                 break;
@@ -132,26 +129,15 @@ public class Instructions_UI : MonoBehaviour
 					enemy.SetActive(true);
 					ranOnce = true;
 				}
-                if (Input.GetKeyDown("tab"))
-                {
-                    state = InstructionState.AttackSwordInstruction;
-                }
                 break;
 
 			case InstructionState.AttackSwordInstruction:
 				textObj.GetComponent<Text>().text = "Go to the enemy and click in order to attack";
-				if (Input.GetKeyDown("tab"))
-				{
-					state = InstructionState.ScoreInstruction;
-				}
+				
 				break;
 
 			case InstructionState.ScoreInstruction :
                 textObj.GetComponent<Text>().text = "Good Luck!!";
-                if (Input.GetKeyDown("tab"))
-                {
-                    SceneManager.LoadSceneAsync("MainScene");
-                }
                 break;
 
         }
@@ -163,36 +149,19 @@ public class Instructions_UI : MonoBehaviour
 
         timer = GameObject.FindGameObjectWithTag("timer");
         if (timer != null) timer.GetComponent<Text>().text = "Time Remaining :  " + ((int)levelTime).ToString();
-
-        /*
-        // this shouldn't be necessary but... it was buggy so here we are
-        textObj = GameObject.FindGameObjectWithTag("Notifications");
-
-        coinHud = GameObject.FindGameObjectWithTag("coin_hud");
-        endScreen = GameObject.FindGameObjectWithTag("end_screen");
-        character = GameObject.FindGameObjectWithTag("Player");
-
-        string message = character.gameObject.GetComponent<Rigidbody>().velocity.ToString();
-
-
-        // makle the text updates
-        if (textObj != null) textObj.GetComponent<Text>().text = message;
-
-        if (character != null) coinNum = character.GetComponent<Coin_Counter>().getCoinCount() * 5;
-        if (coinHud != null) coinHud.GetComponent<Text>().text = coinNum.ToString();
-
-        endScreen.GetComponent<Text>().text =
-            "Congratulations!!\n" +
-            "You won the game with score of " + finalScore() + ".\n" +
-            "Try again to beat this score!";
-
-        levelTime -= Time.deltaTime;
-
-        if (levelTime < LEVEL_END_TIME)
+        
+        if (Input.GetKeyDown("tab"))
         {
-            // restart level
+            stateNum++;
+        } else if (Input.GetKeyDown("backspace"))
+        {
+            stateNum--;
         }
-        */
+
+        if (stateNum == states.Length)
+        {
+            SceneManager.LoadSceneAsync("MainScene");
+        }
     }
 
 }
