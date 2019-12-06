af6cc28b3a8c1260743d74f5e7ddc3abad4a054b
isabelle20011 <isabelle.turnes@hotmail.com>
2019-12-05 20:11:05 -0500

    FIXED - bug where you explode hooking from the platform

--- SolarFlare/Assets/attachPlayer.cs
+++ SolarFlare/Assets/attachPlayer.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class attachPlayer : MonoBehaviour
 {
+
+	private GrapplingHook grappling;
+	private GameObject player;
+	private bool attached;
+
+	private void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (player)
+		{
+			grappling = player.GetComponent<GrapplingHook>();
+		}
+		if (!grappling)
+		{
+			Debug.LogWarning("No grappling hook script found");
+		}
+	}
+
+	private void Update()
+	{
+		if (attached)
+		{
+			if (grappling)
+			{
+				if (grappling.hooked)
+				{
+					player.transform.parent = null;
+					attached = false;
+				}
+			}
+		}
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
 			other.transform.parent = transform;
+			attached = true;
 		}
 	}
 
@@ -17,6 +51,7 @@ public class attachPlayer : MonoBehaviour
 		if (other.CompareTag("Player"))
 		{
 			other.transform.parent = null;
+			attached = false;
 		}
 	}
 }
